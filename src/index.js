0353b3d594a1d656c395ad4144bf53691bbbc824
MaximNic <maxlabnk@gmail.com>
2018-02-23 09:03:59 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,55 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.arr = [];    // arr
+    
+    
   }
 
   add(element) {
-    // your implementation
+    this.arr.push(element);
+
+    
   }
 
   at(index) {
-    // your implementation
+    this.arr[index];    // your implementation
   }
 
   get length() {
-    // your implementation
+    return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+    return this.arr;
   }
 
   sort(indices) {
-    // your implementation
+    
+    this.start=indices.sort((a,b)=>a-b)[0];
+    this.end=indices.sort((a,b)=>b-a)[0]+1;
+    this.sorty=this.arr.slice( this.start , this.end);
+    this.sorty.sort ((a,b)=>a-b);
+    for (this.i=0; this.i<this.end-this.start; this.i++){  //{}
+      this.arr[this.i+this.start]=this.sorty[this.i];
+    } 
+      return this.arr;
+    
+
+/*      start=indices.sort((a,b)=>a-b)[0];
+      end=indices.sort((a,b)=>b-a)[0]+1;
+      sorty=arr.slice( start , end);
+      console.log(sorty.sort ((a,b)=>a-b));
+      for (i=0; i<end-start; i++)
+        arr[i+start]=sorty[i];
+      console.log(arr);
+
+
+
+   */
+    //this.arr.slice(indices.)
+    //this.arr.concat ( this.arr.slice(0,this.start), this.arr.sort ( this.arr.slice( this.start , this.end)) ,  this.arr.slice(this.end, this.arr.length-1));
+       // this.ind=[];
+    //this.arr.sort[indices]        // your implementation
   }
 
   setComparator(compareFunction) {
